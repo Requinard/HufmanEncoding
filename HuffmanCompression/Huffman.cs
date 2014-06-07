bd606a9453374f9a3365d17bbe927ea68607e640
samynv <d.diks@mail.blastedsolutions.com>
2014-06-07 12:19:39 +0200

    added string evaluation

--- HuffmanCompression/Huffman.cs
+++ HuffmanCompression/Huffman.cs
@@ -32,11 +32,40 @@ namespace HuffmanCompression
             this.value = value;
         }
 
+        /// <summary>
+        /// Creates an entire huffman tree based on a string that needs to be compressed
+        /// </summary>
+        /// <param name="toCompress">String that needs compressing</param>
+        /// <returns>Complete Huffman priority queue</returns>
         public HuffmanTree createTree(string toCompress)
         {
-
+            Dictionary<char, int> weightDictionairy = createDictionairy(toCompress);
 
             return this;
+        }
+
+        /// <summary>
+        /// Compresses a string into a dictionairy holding the weight of each character
+        /// </summary>
+        /// <param name="s">String that needs to be compressed</param>
+        /// <returns>Dictionairy containing weight for each character in a string</returns>
+        private Dictionary<char, int> createDictionairy(string s)
+        {
+            Console.WriteLine("Starting string compression");
+            Dictionary<char, int> dict = new Dictionary<char, int>();
+
+            Console.WriteLine("Starting iterating through string")
+            foreach (char c in s)
+            {
+                if (dict.ContainsKey(c))
+                    dict[c] += 1;
+                else
+                    dict[c] = 1;
+
+                Console.WriteLine("Char %s has appeared %d times", c, dict[c]);
+            }
+
+            return dict;
         }
     }
 }
